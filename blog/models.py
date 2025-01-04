f4c8fb5c1991655af3f835357f0a214418775621
m2g_prince <princepatelm2g@gmail.com>
2025-01-04 13:48:15 +0530

    seting static file for defult

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.templatetags.static import static
 
 class Tag(models.Model):
     title=models.CharField(max_length=20)
@@ -16,8 +16,9 @@ class Blog(models.Model):
     slug=models.SlugField(unique=True)
     tags=models.ManyToManyField(Tag,related_name='blogs')
 
-    def get_thumbnai_url(self):
-        return self.thumbnai.url if self.thumbnai else '/media/blogs/defult_blog.png'
+    def get_thumbnai(self):
+        print(static('images/defult_images/defult_blog.png'))
+        return self.thumbnai.url if self.thumbnai else static('images/defult_images/defult_blog.png')
 
     def __str__(self):
         return self.title
